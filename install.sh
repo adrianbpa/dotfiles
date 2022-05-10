3be69d996d0661612b73c5be4367bc5fbc0de9bc
Adrian Bulnes <adrian@anomalo.com>
2022-05-10 16:24:45 -0700

    Bash updates.

--- install.sh
+++ install.sh
@@ -10,14 +10,21 @@ BASEDIR=$(dirname "$SCRIPT")
 ln -sf ${BASEDIR}/linux/personal_config/files/p10k.zsh.sh ${HOME}/.p10k.zsh
 ln -sf ${BASEDIR}/linux/personal_config/files/zpreztorc.sh ${HOME}/.zpreztorc
 ln -sf ${BASEDIR}/linux/personal_config/files/zshrc.sh ${HOME}/.zshrc
+ln -sf ${BASEDIR}/linux/personal_config/files/bashrc.sh ${HOME}/.bashrc
+chmod +x ${HOME}/.bashrc
 
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git --branch 0.29.0 ~/.fzf
 ~/.fzf/install --all --no-bash
 
-chsh --shell /usr/bin/zsh
+#chsh --shell /usr/bin/zsh
+chsh --shell /bin/bash
 
 # git config
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global pull.rebase true
 git config --global push.default current
+
+apt update -y
+apt install nano -y
+apt install -y bash-completion
---
+++ linux/personal_config/files/bashrc.sh
@@ -0,0 +1,2 @@
+alias ll='ls $LS_OPTIONS -lah'
+alias k="kubectl"
