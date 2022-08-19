67500e13839ad6d97486247a5cabb3179fe6bfb1
Adrian Bulnes <adrian@anomalo.com>
2022-08-19 20:53:15 +0200

    Script updates

--- linux/LOCAL_PATH/abp_deploy_k8s.sh
+++ linux/LOCAL_PATH/abp_deploy_k8s.sh
@@ -2,11 +2,20 @@
 
 set -xeuo pipefail
 
+die() { echo "ERROR: $*" 1>&2 ; exit 1; }
+
+echo -e "Deleting old resources..."
+# kubectl delete deployments/datadog-cluster-agent -n sandbox-a6h9
+# kubectl delete daemonset datadog -n sandbox-a6h9
+# kubectl delete service datadog-custom-metrics-server -n sandbox-a6h9
+# kubectl delete APIService v1beta1.external.metrics.k8s.io
+# kubectl delete hpa datadog-hpa-dquality-celery-queue-length -n sandbox-a6h9
+
 pushd /workspaces/dquality/deploy/kubernetes/
 echo -e "Generating k8s YAML..."
-./make-customer-yaml.sh anomalo-sandbox overlays/sandbox-a6h9
+./make-customer-yaml.sh anomalo-sandbox overlays/sandbox-a6h9 || die "Make Customer YAML failed"
 echo -e "Using temporary Docker image from AWS ECR instead of prod image from GCR..."
 sed -i 's/us-docker.pkg.dev\/anomalo-invpc\/anomalo-sandbox\/deploy-web:anomalo-sandbox/580663733917.dkr.ecr.us-west-1.amazonaws.com\/dquality\/deploy-web:anomalo-sandbox/' anomalo.yaml
 echo -e "Kubectl Applying k8s resources..."
-kubectl --context arn:aws:eks:us-west-2:580663733917:cluster/anomalo-staging --namespace sandbox-a6h9 apply -f anomalo.yaml
+kubectl --context arn:aws:eks:us-west-2:580663733917:cluster/anomalo-staging --namespace sandbox-a6h9 apply -f anomalo.yaml || die "Kubectl apply failed"
 popd
--- linux/LOCAL_PATH/abp_deploy_new_container_to_k8s.sh
+++ linux/LOCAL_PATH/abp_deploy_new_container_to_k8s.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-set -xeuo pipefail
+die() { echo "ERROR: $*" 1>&2 ; exit 1; }
 
 pushd /workspaces/dquality/
 
-echo -e "Building dquality-web Docker container..."
-deploy/docker/build.sh web
+if [[ "${1-""}" == "web" ]]; then
+  BUILD_SCRIPT_ID="web"
+  DOCKER_IMG_ID="dquality/deploy-web"
+elif [[ "${1-""}" == "1cd" ]]; then
+  BUILD_SCRIPT_ID="one-click-deployer"
+  DOCKER_IMG_ID="anomalo/oneclickdeployer"
+else
+  die "First argument must be 'web' or '1cd', but it's '${1-(empty)}'"
+fi
 
-echo -e "Retagging dquality-web as deploy-web:anomalo-sandbox..."
-docker tag 580663733917.dkr.ecr.us-west-1.amazonaws.com/dquality/deploy-web:$(git rev-parse --abbrev-ref HEAD) 580663733917.dkr.ecr.us-west-1.amazonaws.com/dquality/deploy-web:anomalo-sandbox
+echo -e "Deleting existing containers"
+docker image rm 580663733917.dkr.ecr.us-west-1.amazonaws.com/${DOCKER_IMG_ID}:$(git rev-parse --abbrev-ref HEAD)
+docker image rm 580663733917.dkr.ecr.us-west-1.amazonaws.com/${DOCKER_IMG_ID}:anomalo-sandbox
+
+set -xeuo pipefail
+
+echo -e "Building ${DOCKER_IMG_ID} Docker container..."
+deploy/docker/build.sh ${BUILD_SCRIPT_ID}
+
+echo -e "Retagging ${DOCKER_IMG_ID} as deploy-web:anomalo-sandbox..."
+docker tag 580663733917.dkr.ecr.us-west-1.amazonaws.com/${DOCKER_IMG_ID}:$(git rev-parse --abbrev-ref HEAD) 580663733917.dkr.ecr.us-west-1.amazonaws.com/${DOCKER_IMG_ID}:anomalo-sandbox
 
 echo -e "Pushing Docker image..."
-docker push 580663733917.dkr.ecr.us-west-1.amazonaws.com/dquality/deploy-web:anomalo-sandbox
+docker push 580663733917.dkr.ecr.us-west-1.amazonaws.com/${DOCKER_IMG_ID}:anomalo-sandbox
 
-clear
+#clear
 echo -e "Available Docker images:"
-docker image ls --digests | grep "web"
+docker image ls --digests | grep $DOCKER_IMG_ID
 
 echo -e "Deploying to k8s..."
 abp_deploy_k8s.sh
