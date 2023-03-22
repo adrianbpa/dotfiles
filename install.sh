05c07450236c0692b2d2e9e2232d4d8115e71852
Adrian B <github@urriellu.net>
2023-03-22 16:48:04 -0700

    Update codesaces install

--- install.sh
+++ install.sh
@@ -1,25 +1,39 @@
 # THIS IS AUTOMATICALLY CALLED BY GITHUB CODESPACES WHEN REBUILDING THE CONTAINER
 
-SCRIPT=$(readlink -f "$0")
-BASEDIR=$(dirname "$SCRIPT")
+set -euo pipefail
 
-ln -sf ${BASEDIR}/linux/personal_config/files/bashrc.sh ${HOME}/.bashrc
-chmod +x ${HOME}/.bashrc
+echo -e "Setting up dotfiles in GitHub Codespaces..."
 
-mkdir ~/.local_path
-cp -a ${BASEDIR}/linux/LOCAL_PATH/* ~/.local_path
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOTFILES_REPO_ROOT=$(realpath $SCRIPT_DIR/../../../)
 
-curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-echo 'source ~/.git-prompt.sh' >> ~/.bashrc
+echo "Script dir: $SCRIPT_DIR"
+echo "Repo root: $DOTFILES_REPO_ROOT"
 
-# install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git --branch 0.29.0 ~/.fzf
-~/.fzf/install --all --no-bash
+pushd $DOTFILES_REPO_ROOT
 
-#chsh --shell /usr/bin/zsh
-chsh --shell /bin/bash
+$DOTFILES_REPO_ROOT/all_users/all_OSs/on_user_login.sh
+
+if [[ -f ~/.bashrc ]]; then
+    if [[ ! -f ~/.bashrc.bck ]]; then
+        mv ~/.bashrc ~/.bashrc.bck
+    else
+        rm ~/.bashrc
+    fi
+fi
+
+echo "# Dynamically generated bashrc from dotfiles" > ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
+cat $DOTFILES_REPO_ROOT/all_users/all_OSs/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
+cat $DOTFILES_REPO_ROOT/all_users/linux/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
+cat $DOTFILES_REPO_ROOT/anomalo/all_OSs/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
 
 # git config
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global pull.rebase true
 git config --global push.default current
+
+mkdir ~/.local_path || echo "~/.local_path already exists"
+cp -a $DOTFILES_REPO_ROOT/anomalo/all_OSs/LOCAL_PATH/* ~/.local_path
+chmod +x ~/.local_path/*
+
+popd
