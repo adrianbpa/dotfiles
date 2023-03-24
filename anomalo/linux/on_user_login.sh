6c31732c92746a7f675d51868c9958467093f432
Adrian Bulnes <104385186+adrianbpa@users.noreply.github.com>
2023-03-24 12:12:10 -0700

    Anomalo: Split codespaces vs non-codespaces linux startup scripts

---
+++ anomalo/linux/on_user_login.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo -e "Setting up dotfiles for Anomalo on Linux..."
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOTFILES_REPO_ROOT=$(realpath $SCRIPT_DIR/../../)
+
+pushd $DOTFILES_REPO_ROOT
+
+$DOTFILES_REPO_ROOT/all_users/all_OSs/on_user_login.sh
+
+if [[ -f ~/.bashrc ]]; then
+    if [[ ! -f ~/.bashrc.bck ]]; then
+        mv ~/.bashrc ~/.bashrc.bck
+    else
+        rm ~/.bashrc
+    fi
+fi
+
+echo "# Dynamically generated bashrc from dotfiles" > ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
+cat $DOTFILES_REPO_ROOT/all_users/all_OSs/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
+cat $DOTFILES_REPO_ROOT/all_users/linux/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
+cat $DOTFILES_REPO_ROOT/anomalo/all_OSs/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
+cat $DOTFILES_REPO_ROOT/anomalo/linux/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
+
+# git config
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global pull.rebase true
+git config --global push.default current
+
+mkdir ~/.local_path || echo "~/.local_path already exists"
+cp -a $DOTFILES_REPO_ROOT/anomalo/all_OSs/LOCAL_PATH/* ~/.local_path
+chmod +x ~/.local_path/*
+
+popd
--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo "Dotfiles repo root: $DOTFILES_REPO_ROOT"
 
 pushd $DOTFILES_REPO_ROOT
 
-$DOTFILES_REPO_ROOT/all_users/all_OSs/on_user_login.sh
+$DOTFILES_REPO_ROOT/linux/on_user_login.sh
 
 if [[ -f ~/.bashrc ]]; then
     if [[ ! -f ~/.bashrc.bck ]]; then
@@ -22,21 +22,6 @@ if [[ -f ~/.bashrc ]]; then
         rm ~/.bashrc
     fi
 fi
-
-echo "# Dynamically generated bashrc from dotfiles" > ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
-cat $DOTFILES_REPO_ROOT/all_users/all_OSs/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
-cat $DOTFILES_REPO_ROOT/all_users/linux/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
-cat $DOTFILES_REPO_ROOT/anomalo/all_OSs/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
-cat $DOTFILES_REPO_ROOT/anomalo/linux/bashrc.sh >> ~/.bashrc && echo -e "\n\n\n" >> ~/.bashrc
-
-# git config
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-git config --global pull.rebase true
-git config --global push.default current
-
-mkdir ~/.local_path || echo "~/.local_path already exists"
-cp -a $DOTFILES_REPO_ROOT/anomalo/all_OSs/LOCAL_PATH/* ~/.local_path
-chmod +x ~/.local_path/*
 
 apt update
 apt install -y iputils-ping dnsutils nano || echo "Error while installing Debian packages"
