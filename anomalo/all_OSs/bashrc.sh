7ab09d1810ff61e6a29eb240bd204733c33225f2
Adrian Bulnes <adrian@anomalo.com>
2023-03-24 12:01:54 -0700

    Anomalo: Reorganizing kubectl aliases

--- anomalo/all_OSs/bashrc.sh
+++ anomalo/all_OSs/bashrc.sh
@@ -3,18 +3,21 @@
 export CONTEXT_DEV="arn:aws:eks:us-west-2:580663733917:cluster/anomalo-staging"
 export CONTEXT_STAGING="arn:aws:eks:us-west-1:580663733917:cluster/anomalo-staging-us-west-1"
 export CONTEXT_PROD="arn:aws:eks:us-west-1:580663733917:cluster/anomalo-production-us-west-1"
+export NAMESPACE_SANDBOX_ADRIAN="sandbox-adrian"
+export NAMESPACE_STAGING="staging-5fa9"
 
 alias k9s_dev="k9s --context $CONTEXT_DEV"
 alias k9s_staging="k9s --context $CONTEXT_STAGING"
 alias k9s_prod="k9s --context $CONTEXT_PROD"
-alias kcluster_demosandbox="kubectl --context arn:aws:eks:us-west-2:580663733917:cluster/anomalo-staging"
-alias ksandbox="kcluster_demosandbox --namespace sandbox-a6h9"
-alias kdemo="kcluster_demosandbox --namespace demo"
-alias kcluster_staging="kubectl --context arn:aws:eks:us-west-1:580663733917:cluster/anomalo-staging-us-west-1"
-alias kstaging="kcluster_staging --namespace staging-5fa9"
-alias k="ksandbox"
-alias ksandbox_all='clear && ksandbox get all,ing -o wide'
-alias ksandbox_setcontext="kubectl config set-context arn:aws:eks:us-west-2:580663733917:cluster/anomalo-staging --namespace=sandbox-a6h9"
+
+alias kdev="kubectl --context $CONTEXT_DEV"
+alias kstaging="kubectl --context $CONTEXT_STAGING"
+
+alias kdev_sandbox="kdev --namespace $NAMESPACE_SANDBOX_ADRIAN"
+alias kstaging_staging="kstaging --namespace $NAMESPACE_STAGING"
+
+alias kdev_all='clear && kdev get all,ing -o wide'
+alias kdev_sandbox_all='clear && kdev_sandbox get all,ing -o wide'
 alias kstaging_all='clear && kstaging get all,ing -o wide'
 
 chsh --shell /bin/bash
