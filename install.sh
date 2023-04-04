164e884e02b67779b25c0943623e044181684e58
Adrian B <github@urriellu.net>
2023-04-03 17:17:56 -0700

    Codespaces fix run on_user_login

--- install.sh
+++ install.sh
@@ -13,15 +13,7 @@ echo "Dotfiles repo root: $DOTFILES_REPO_ROOT"
 
 pushd $DOTFILES_REPO_ROOT
 
-$DOTFILES_REPO_ROOT/linux/on_user_login.sh
-
-if [[ -f ~/.bashrc ]]; then
-    if [[ ! -f ~/.bashrc.bck ]]; then
-        mv ~/.bashrc ~/.bashrc.bck
-    else
-        rm ~/.bashrc
-    fi
-fi
+$DOTFILES_REPO_ROOT/anomalo/linux/on_user_login.sh
 
 apt update
 apt install -y iputils-ping dnsutils nano || echo "Error while installing Debian packages"
