b536cfc7abfdefcee659f18be6a342d4a4bf2b7c
Adrian B <github@urriellu.net>
2023-04-04 13:10:48 -0700

    Export DOTNET_ROOT on codespaces

--- anomalo/linux/bashrc.sh
+++ anomalo/linux/bashrc.sh
@@ -1,3 +1,9 @@
 # BASHRC FOR ANOMALO, LINUX
 
 export PATH=$PATH:~/.local_path
+
+if [[ "${CODESPACES:""}" != "true" ]]; then
+    export DOTNET_ROOT="/usr/share/dotnet"
+    [[ ! -d "$DOTNET_ROOT" ]] && echo "ERROR: Missing .NET in ${DOTNET_ROOT} - Please install from https://learn.microsoft.com/en-us/dotnet/core/install/linux-debian"
+    export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+fi
